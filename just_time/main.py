5de09e33f44bfe586d2de81e663af646d3b828b6
unknown <zznixt07@protonmail.com>
2021-05-09 19:21:04 +0545

    removed unncessary codes

--- just_time/main.py
+++ just_time/main.py
@@ -10,7 +10,7 @@ class JustTime:
         tzinfo: tzinfo = timezone.utc):
         '''JustTime allows representing time greater than 24hrs.
         If time is greater than 24hrs then only the hour will increase past its
-        normal limit of <=23. Minute, second, and microsecond remain under their
+        normal limit of <= 23. Minute, second, and microsecond remain under their
         limit.
         Supports addition and substraction with other JustTime, time and timedelta object.
 
@@ -50,7 +50,6 @@ class JustTime:
         return self._microsecond
     
     def __str__(self) -> str:
-        # 02, max width=2. precede with 0s if width < 2
         return f'{self.hour:02}:{self.minute:02}:{self.second:02}:{self.microsecond:06}'
 
     def __repr__(self) -> str:
@@ -79,6 +78,7 @@ class JustTime:
                
     def __add__(self, other) -> JustTime:
         "TODO support tzinfo"
+
         other_obj: Union[time, JustTime]
         if isinstance(other, (time, JustTime)):
             other_obj = other
@@ -95,9 +95,8 @@ class JustTime:
         )
 
     def __sub__(self, other) -> Union[timedelta, JustTime]:
-        # t-t -> delta
-        # t-delta -> t
         "TODO support tzinfo"
+
         other_obj: Union[time, JustTime]
         if isinstance(other, (time, JustTime)):
             # return timedelta(seconds=self.total_seconds()-int(other.total_seconds()))
@@ -142,12 +141,10 @@ class JustTime:
         return self._total_microseconds
 
     def lies_between(self, starting, ending) -> bool:
-        if self.total_microseconds() in range(
+        return bool(
+            self.total_microseconds() in range(
                 starting.total_microseconds(), ending.total_microseconds()
-            ):
-            return True
-        else:
-            return False
+        ))
 
 
 if __name__ == '__main__':
@@ -156,11 +153,10 @@ if __name__ == '__main__':
     t2 = JustTime(23, 59, 59)
     print(t1.strftime('%I:%M %p'))
     print(t2.strftime('%I:%M %p'))
-    print(t2)
-    # print(t1.hour)
-    # print(t1.minute)
-    # print(t1.second)
-    # print(t1.total_seconds())
+    print(t1.hour)
+    print(t1.minute)
+    print(t1.second)
+    print(t1.total_seconds())
 
     # print(t1 + t2)
     # print(t1 - t2)
@@ -199,95 +195,3 @@ if __name__ == '__main__':
     # st_diff = other_st.total_microseconds() - mine_st.total_microseconds()
     # end_diff = mine_end.total_microseconds() - other_end.total_microseconds()
 
-    # first check if starting time lies in range
-    if other_st.lies_between(mine_st, mine_end):
-        st_diff = range(mine_st.total_microseconds(), other_st.total_microseconds())
-        end_diff = range(other_end.total_microseconds(), mine_end.total_microseconds())
-        if st_diff:
-            gaps.append(st_diff)
-        if end_diff:
-            if end_diff[-1] - end_diff[0] > mine_st.total_microseconds() - mine_end.total_microseconds():
-                gaps.append(range(mine_st.total_microseconds(), mine_end.total_microseconds()))
-            else:
-                gaps.append(end_diff)
-
-    from pprint import pprint
-    # print(gaps)
-    # pprint([(JustTime(microsecond=n[0]), JustTime(microsecond=n[-1])) for n in gaps])
-    # if other_st.total_microseconds() != mine_st.total_microseconds():
-
-
-    from typing import List, Tuple
-
-    def bits_to_time_ranges(time_begin: JustTime, bits: str) -> List[Tuple(JustTime, JustTime)]:
-
-        def helper(secs: int): return time_begin + timedelta(seconds=secs)
-
-        time_ranges: List[Tuple(JustTime, JustTime)] = []
-        offset: int = 0
-        bit_set: bool = False
-
-        for i in range(len(bits)):
-            if bits[i] == '1':
-                if not bit_set:
-                    bit_set = True
-                    offset = i
-            else:
-                if bit_set:
-                    bit_set = False
-                    time_ranges.append((helper(offset), helper(i)))
-        else:
-            if bit_set:
-                time_ranges.append((helper(offset), helper(i)))
-
-        return time_ranges
-
-    def bitwise_and(p1: str, p2: str) -> str:
-        shortest_len: int = len(p1) if len(p1) < len(p2) else len(p2)
-        return ''.join([str(int(p1[i]) & int(p2[i])) for i in range(shortest_len)])
-
-    lst = [
-        (JustTime(9, 15), JustTime(9, 25)),
-        (other_st, other_end),
-    ]
-    gaps = []
-    for other_st, other_end in lst:
-        if (not other_st.lies_between(mine_st, mine_end)
-            and not other_end.lies_between(mine_st, mine_end)
-            ):
-            # both ends lie outside the range. No collision.
-            continue
-
-        dark_matter_length = 0
-        # at most, 1 end is outside the range.
-        if (other_st - mine_st).total_seconds() > 0:
-            left_diff = int(float((other_st - mine_st).total_seconds()))
-        else:
-            left_diff = 0
-            dark_matter_length = int(float((other_end - mine_st).total_seconds()))
-
-        if (mine_end - other_end).total_seconds() > 0:
-            right_diff = int(float((mine_end - other_end).total_seconds()))
-        else:
-            right_diff = 0
-            dark_matter_length = int(float((mine_end - other_st).total_seconds()))
-
-        # print(f'{left_diff=} {right_diff=}')
-        if dark_matter_length <= 0:
-            dark_matter_length = int(float((other_end - other_st).total_seconds()))
-        dark_matter = (
-            left_diff * '1'
-            # + int(float((other_end - other_st).total_seconds())) * '0'
-            + dark_matter_length * '0'
-            + right_diff * '1'
-        )
-
-        # print(dark_matter,'dark_matter')
-        normal_matter = int(float((mine_end - mine_st).total_seconds())) * '1'
-        bits: str = bitwise_and(normal_matter, dark_matter)
-        # print(bits)
-        
-        gaps.extend(bits_to_time_ranges(mine_st, bits))
-
-    print(gaps)
-    duplicates_removed = []
